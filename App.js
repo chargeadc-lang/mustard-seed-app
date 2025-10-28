d9df14ef389016e974f2273a8eebc4c020cf9124
chargeadc-lang <chargeadc@gmail.com>
2025-10-28 13:02:46 -0400

    Repair: navigation + data flow logic

--- App.js
+++ App.js
@@ -1,21 +1,5 @@
-// App.js
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screens/HomeScreen";
-import ProgressScreen from "./screens/ProgressScreen";
-import CalendarScreen from "./screens/CalendarScreen"; // make sure this file exists
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="ProgressScreen" component={ProgressScreen} />
-        <Stack.Screen name="CalendarScreen" component={CalendarScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// rest of App.js code here
